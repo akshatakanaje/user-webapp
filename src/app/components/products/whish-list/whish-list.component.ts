d5013ea1b419e4ccc1589eaed9e27d82fdc4f6a6
Akshata Kanaje <akshatakanaje@Akshatas-MacBook-Pro.local>
2022-12-07 16:41:00 -0800

    added cartlist, wishlist and checkout functionality

--- src/app/components/products/whish-list/whish-list.component.ts
+++ src/app/components/products/whish-list/whish-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/service/products.service';
 
 @Component({
   selector: 'app-whish-list',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhishListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public productsService: ProductsService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  removeProductFromWhishlist(prdIdx:number) {
+    let elements = this.productsService.whishlistProducts.splice(prdIdx, 1);
+    console.log(elements[0]['title'], "Product Removed from Whishlist");
+  }
+
+  addProductToShoppingCart(prd:any, removeBool:boolean, prdIdx:number) {
+    this.productsService.addProductToShoppingCart(prd, removeBool, prdIdx);
+    this.removeProductFromWhishlist(prdIdx);
   }
 
 }
